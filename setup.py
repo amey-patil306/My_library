48ebddb09a2a7ddf61b0d3fc7e279ed4d2ba1bd3
amey-patil306 <138242356+amey-patil306@users.noreply.github.com>
2024-05-05 21:45:43 +0530

    hi

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sklean_306',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies your library requires
+    ],
+    author='Mr Unknow',
+    author_email='itachiuchiha106106106@email.com',
+    description='Description of your library',
+    url='https://github.com/amey-patil306/My_library',
+    license='MIT',
+    keywords='your, keywords, here',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+)
